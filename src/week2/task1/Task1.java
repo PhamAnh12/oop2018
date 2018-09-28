3dbbda4dd7c510b38ba83657dbfaab909c73b745
PhamAnh99 <phamcongtuananh99@gmail.com>
2018-09-28 11:01:49 +0700

    week2

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,40 @@
 package week2.task1;
-
 public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (a != b) {
+            if (a > b) {
+                a -= b;
+            } else {
+                b -= a;
+            }
+        }
+        return a;
+
+
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        else {
+
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(week2.task1.Task1.gcd(8, 10));
+        System.out.println(week2.task1.Task1.fibonacci(6));
+
     }
 }
