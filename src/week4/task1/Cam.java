4bbd5eec9a5035fa603c5a603cb3dd7e3db38765
PhamAnh99 <phamcongtuananh99@gmail.com>
2018-10-11 22:57:41 +0700

    abc
    l

---
+++ src/week4/task1/Cam.java
@@ -0,0 +1,46 @@
+package week4.task1;
+
+public class Cam extends HoaQua {
+    private String mau;
+    private long sanluong;
+
+    public String getMau() {
+        return mau;
+    }
+
+    public void setMau(String mau) {
+        this.mau = mau;
+    }
+
+    public long getSoluong() {
+        return sanluong;
+    }
+
+    public void setSoluong(long soluong) {
+        this.sanluong = soluong;
+    }
+
+    public Cam() {
+
+    }
+
+    public Cam(String mau, long sanluong) {
+        mau = "Xanh";
+        sanluong = 123456;
+    }
+    public double nangxuat(double dientich){
+        return sanluong/dientich;
+    }
+
+
+    @Override
+    public void ShowInfo() {
+        super.ShowInfo();
+        System.out.println("Mau cua Cam: " + mau);
+        System.out.println("San Luong cua Cam: " + sanluong);
+    }
+
+
+
+
+}
---
+++ src/week4/task1/Tao.java
@@ -0,0 +1,40 @@
+package week4.task1;
+
+public class Tao extends HoaQua {
+    private String mau;
+    private long sanluong;
+
+    public String getMau() {
+        return mau;
+    }
+
+    public void setMau(String mau) {
+        this.mau = mau;
+    }
+
+    public long getSoluong() {
+        return sanluong;
+    }
+
+    public void setSoluong(long soluong) {
+        this.sanluong = soluong;
+    }
+
+    public Tao(){
+        mau = "red";
+        sanluong = 399999;
+    }
+
+    public boolean Dachinchua(){
+        if(mau == "red")
+            return true;
+        return false;
+    }
+
+    @Override
+    public void ShowInfo() {
+        super.ShowInfo();
+        System.out.println("Mau cua Tao: " + mau);
+        System.out.println("San luong cua Tao: " + sanluong);
+    }
+}
