a7d35befb93b920f180916bd83a88d05e4a8f1ad
PhamAnh99 <phamcongtuananh99@gmail.com>
2018-11-16 10:40:16 +0700

    week 10

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,83 @@
 package week10;
 
-public class Task1 {
-}
+  import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+    public class Task1 {
+
+        // List method
+        private List<String> list;
+
+        public List<String> getAllFunctions(File path) throws IOException {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            list = new ArrayList<>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains("/*")) {
+                    while (!line.contains("*/")) {
+                        line = br.readLine();
+                    }
+                }
+                if (line.contains("static") && line.endsWith("{")) {
+                    String str = "";
+                    str += (line + "\n");
+                    while ((line = br.readLine()) != null) {
+                        String str2 = line;
+                        str += (str2 + "\n");
+                        if (str2.endsWith("}")) {
+                            break;
+                        }
+                    }
+                    list.add(str);
+                }
+            }
+            return list;
+        }
+
+        public void showAllList() {
+            list.forEach((lists) -> {
+                System.out.println(lists);
+            });
+        }
+
+        public String findFunctionByName(String name) throws IOException {
+            BufferedReader br = new BufferedReader(new FileReader("./src/Week10/Utils.java"));
+            boolean checkFound = false;
+            String line;
+            String str = "";
+            while ((line = br.readLine()) != null) {
+                if (line.contains(name) && line.endsWith("{")) {
+                    str += (line + "\n");
+                    while ((line = br.readLine()) != null) {
+                        String str2 = line;
+                        str += (str2 + "\n");
+                        if (str2.endsWith("}")) {
+                            break;
+                        }
+                    }
+                    checkFound = true;
+                }
+            }
+            if (checkFound) {
+                return str;
+            } else {
+                return "Method not found!";
+            }
+        }
+
+        public static void main(String[] args) throws IOException {
+            String name = "SystemOut";
+            Task1 task1 = new Task1();
+            File files = new File("./src/Week10/Utils.java");
+            System.out.println("Tất cả hàm static trong file Utlis: ");
+            task1.getAllFunctions(files);
+            task1.showAllList();
+            System.out.println("Tìm hàm: " + name);
+            System.out.println("Hàm cần tìm: ");
+            System.out.println(task1.findFunctionByName(name));
+        }
+    }
+
+
+
--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,36 @@
 package week10;
 
+import java.util.Random;
+
 public class Task2 {
+    public  void  sapxepnoibot(float a[], int n){
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-1; j++){
+                if(a[j] > a[j+1]){
+                    float x = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = x;
+
+                }
+            }
+        }
+    }
+
+    public static
+    void main(String[] args) {
+        Task2 task2 = new Task2();
+        Random x = new Random();
+        int n = 100;
+        float mang [] = new float[n];
+        for (int i = 0; i < n; i++){
+            mang[i] = x.nextFloat();
+            System.out.println(mang[i]+ " ");
+        }
+        System.out.println( "");
+        task2.sapxepnoibot(mang,n);
+        for (int i = 0; i < n; i++){
+            System.out.println(mang[i]+ " ");
+        }
+
+    }
 }
