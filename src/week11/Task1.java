49c2a7ccf3f6f9f6c75da2b43f3c72a4bd3294d0
PhamAnh99 <phamcongtuananh99@gmail.com>
2018-11-22 21:43:50 +0700

    Week 11

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,44 @@
 package week11;
 
+
 public class Task1 {
+    public <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public <T extends Comparable<T>> void sort(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Task1 task1 = new Task1();
+
+        Integer[] intArray = { 1, 10, 8, 4, 5, 3 };
+        Double[] doubleArray = { 1.154, 2.5, 8.9, 4.36, 5.20, 3.15 };
+        Character[] charArray = { 'a', 's', 'm', 'k', 'b', 'z' };
+
+        task1.sort(intArray);
+        task1.printArray(intArray);
+
+        task1.sort(charArray);
+        task1.printArray(charArray);
+
+        task1.sort(doubleArray);
+       task1.printArray(doubleArray);
+
+
+
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,41 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+    public <T extends Comparable<T>> T maximum( ArrayList<T> arr) {
+        T max = arr.get(0);
+
+        for (T element : arr){
+            if (element.compareTo(max) > 0)
+                max = element;
+        }
+
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+      Task2 task2 = new Task2();
+
+        ArrayList arrayListInt = new ArrayList();
+        arrayListInt.add(5);
+        arrayListInt.add(40);
+        arrayListInt.add(10);
+
+        ArrayList arrayListDouble = new ArrayList();
+        arrayListDouble.add(1.02);
+        arrayListDouble.add(9.99999);
+        arrayListDouble.add(1.024);
+
+        ArrayList arrayListChar = new ArrayList();
+        arrayListChar.add('A');
+        arrayListChar.add('M');
+        arrayListChar.add('C');
+
+        System.out.println("The maximum of ArrayList Integer is: " + task2.maximum(arrayListInt));
+        System.out.println("The maximum of ArrayList Double is: " +task2.maximum(arrayListDouble));
+        System.out.println("The maximum of ArrayList Character is: " + task2.maximum(arrayListChar));
+
+    }
 }
